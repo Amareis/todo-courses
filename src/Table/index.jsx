8aae448c73f50b3640ef16e539df729646ed64b7
Amareis <terma95@gmail.com>
2021-07-29 20:33:26 +0500

    Converted Table to class

--- src/Table/index.jsx
+++ src/Table/index.jsx
@@ -2,70 +2,105 @@ import React from 'react'
 import S from './styles.module.css'
 import Todo from './Todo'
 import {Header} from './Header'
-import {useForceUpdate, useLocalStorage} from '../hooks'
 import {Footer} from './Footer'
 
-export default function Table() {
-  const forceUpdate = useForceUpdate()
-  const [todos, setTodos] = useLocalStorage('todos', [])
-  const [filter, setFilter] = React.useState(null)
+function getLocal(key, obj) {
+  const t = localStorage.getItem(key)
+  if (!t) return obj
+  try {
+    return JSON.parse(t)
+  } catch (e) {
+    return obj
+  }
+}
 
-  const undone = todos.filter((t) => !t.done)
-  const isAllDone = undone.length === 0
+const key = 'todos'
 
-  return (
-    <div className={S.root}>
-      <Header
-        onMarkAll={
-          !todos.length
-            ? null
-            : () => {
-                todos.forEach((t) => (t.done = !isAllDone))
-                forceUpdate()
-              }
-        }
-        onNewTodo={(newTodo) =>
-          setTodos([
-            ...todos,
-            {id: Math.random(), text: newTodo, done: false},
-          ])
-        }
-      />
-      {todos
-        .filter((t) => t.done !== filter)
-        .map((t) => (
-          <Todo
-            key={t.id}
-            text={t.text}
-            done={t.done}
-            onDelete={() =>
-              setTodos(todos.filter((todo) => todo !== t))
-            }
-            onDone={() => {
-              t.done = true
-              forceUpdate()
-            }}
-            onUndone={() => {
-              t.done = false
-              forceUpdate()
-            }}
-            onEdit={(text) => {
-              t.text = text
-              forceUpdate()
-            }}
-          />
-        ))}
-      {todos.length > 0 && (
-        <Footer
-          itemsLeft={undone.length}
-          onFilter={setFilter}
-          onDeleteAll={
-            todos.length === undone.length
-              ? null
-              : () => setTodos(undone)
-          }
+export default class Table extends React.Component {
+  state = {
+    todos: getLocal(key, []),
+    filter: null,
+  }
+
+  get undone() {
+    return this.state.todos.filter((t) => !t.done)
+  }
+
+  markAll = () => {
+    const isAllDone = this.undone.length === 0
+    for (const t of this.state.todos) {
+      t.done = !isAllDone
+    }
+    this.forceUpdate()
+  }
+
+  setTodos = (todos) => {
+    this.setState({todos})
+    localStorage.setItem(key, JSON.stringify(todos))
+  }
+
+  newTodo = (newTodo) => {
+    this.setTodos([
+      ...this.state.todos,
+      {
+        id: Math.random(),
+        text: newTodo,
+        done: false,
+      },
+    ])
+  }
+
+  setFilter = (filter) => this.setState({filter})
+
+  render() {
+    const {todos, filter} = this.state
+
+    const undone = this.undone
+
+    return (
+      <div className={S.root}>
+        <Header
+          onMarkAll={!todos.length ? null : this.markAll}
+          onNewTodo={this.newTodo}
         />
-      )}
-    </div>
-  )
+        {todos
+          .filter((t) => t.done !== filter)
+          .map((t) => (
+            <Todo
+              key={t.id}
+              text={t.text}
+              done={t.done}
+              onDelete={() =>
+                this.setTodos(
+                  todos.filter((todo) => todo !== t),
+                )
+              }
+              onDone={() => {
+                t.done = true
+                this.forceUpdate()
+              }}
+              onUndone={() => {
+                t.done = false
+                this.forceUpdate()
+              }}
+              onEdit={(text) => {
+                t.text = text
+                this.forceUpdate()
+              }}
+            />
+          ))}
+        {todos.length > 0 && (
+          <Footer
+            itemsLeft={undone.length}
+            onFilter={this.setFilter}
+            onDeleteAll={
+              todos.length === undone.length
+                ? null
+                : () => this.setTodos(undone)
+            }
+          />
+        )}
+      </div>
+    )
+  }
 }
